99e5f0ec5d34eda4f880041b86bcd50407aea571
bwaterman8 <bwaterman8@gmail.com>
2021-08-09 13:07:10 -0400

    created student and teacher list and built first school

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Teacher lizzy = new Teacher(1, "Lizzy", 500);
+        Teacher melissa = new Teacher(2, "Melissa", 700);
+        Teacher vanderhorn = new Teacher(3, "Vanderhorn", 5000);
+
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(lizzy);
+        teacherList.add(melissa);
+        teacherList.add(vanderhorn);
+
+        Student tom = new Student(1, "Tom", 9);
+        Student lil = new Student(2, "Lil", 12);
+        Student ron = new Student(3, "Ron", 4);
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(tom);
+        studentList.add(lil);
+        studentList.add(ron);
+
+        School CAHS = new School("CAHS", teacherList, studentList);
+        System.out.println("CAHS has earned a total of $" + CAHS.getTotalIncome());
+
     }
 }
