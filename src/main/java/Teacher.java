9ebc3a93d5c699c906f909cc94bf4ef3147fdce0
bwaterman8 <bwaterman8@gmail.com>
2021-08-09 11:07:59 -0400

    teacher class created

---
+++ src/main/java/Teacher.java
@@ -0,0 +1,31 @@
+public class Teacher {
+    private int id;
+    private String name;
+    private int salary;
+
+    public Teacher(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
